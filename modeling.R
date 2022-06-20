990739d198823458d2cd960deef87528a596fbac
M Earle <you@example.com>
2022-06-19 21:06:09 -0400

    update model

--- modeling.R
+++ modeling.R
@@ -1,9 +1,11 @@
 #install.packages("randomForest")
+install.packages("caret")
 
 train <- read.csv("train.csv", header=TRUE)
 test <- read.csv("test.csv", header = TRUE)
 
-library(randomForest)
+#library(randomForest)
+library(caret)
 
 #for a classification problem, need to make sure that outcome is factor
 train$Survived <- as.factor(train$Survived)
@@ -11,6 +13,14 @@ typeof(train$Age)
 
 #remove missing rows (for now)
 train.narm <- na.omit(train)
+
+####Feature Engineering- TO DO ####
+
+#### Modeling ####
+
+#10-fold cross validation
+control <- 
+
 
 #random forest
 rf <- randomForest(
